34955578404f2de4ede763b2993479b977f5d263
Steve-bankz <nzubenwanochieama@gmail.com>
2025-10-20 21:54:35 +0100

    feat: added rate limiting middleware for API protection

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ import { JSONFile } from "lowdb/node";
 import analyzeRoutes from "./routes/analyzeRoutes.js";
 import 'dotenv/config';
 import fs from 'fs';
+import rateLimit from 'express-rate-limit';
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per window
+  message: {
+    status: 429,
+    error: 'Too Many Requests: please try again later',
+  },
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
 
 const dbPath = './db.json';
 if (!fs.existsSync(dbPath)) {
   fs.writeFileSync(dbPath, JSON.stringify({ analyses: [] }, null, 2));
-  console.log("✅ db.json created automatically");
+  console.log(" db.json created automatically");
 }
 
 
